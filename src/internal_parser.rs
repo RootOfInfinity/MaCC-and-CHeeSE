40a52ca0a2d3680eb5d10e6c45a3fb3c318f51aa
William Campbell <numbersdev@protonmail.com>
2026-06-28 21:57:19 -0500

    created the start of recur descent internal_parser

--- src/internal_parser.rs
+++ src/internal_parser.rs
@@ -1,1 +1,27 @@
-pub struct ParsingEngine;
+use crate::internal_lexer::{Attr, LexEngine, Token};
+
+// Hand-written internal recursive descent parser
+pub struct ParsingEngine {
+    cur_tok: (Token, Attr, (usize, usize)),
+    lex_engine: LexEngine,
+}
+
+impl ParsingEngine {
+    pub fn new_from_lexengine(mut lex_engine: LexEngine) -> Result<Self, &'static str> {
+        let cur_tok = lex_engine
+            .next_tok()
+            .ok_or("No tokens left in lex engine")?;
+        Ok(ParsingEngine {
+            cur_tok,
+            lex_engine,
+        })
+    }
+    fn next_tok(&mut self) -> Result<(), &'static str> {
+        let next_tok = self
+            .lex_engine
+            .next_tok()
+            .ok_or("No tokens left in lex engine")?;
+        self.cur_tok = next_tok;
+        Ok(())
+    }
+}
