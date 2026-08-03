6650b134d49baef7e0d9fd9740e9be3f2b267408
William Campbell <numbersdev@protonmail.com>
2026-08-03 16:24:53 -0500

    added and implemented all_stored_items_in_ast, put terms_to_store in ParserData struct

--- src/data_gleaning.rs
+++ src/data_gleaning.rs
@@ -1,18 +1,39 @@
-use crate::internal_parser::Start;
+use std::collections::HashSet;
+
+use crate::{
+    internal_lexer::{Attr, Token},
+    internal_parser::{Rules, Start, Store},
+};
 
 pub fn data_from_parse(ast: &Start) -> ParserData {
-    let prods = all_productions_in_ast(ast);
+    let Start(store, rules) = ast;
+    let prods = all_productions_in_ast(rules);
+    let terms_to_store = all_stored_terms_in_ast(store);
     let nonterms = all_nonterms_in_productions(&prods);
     let terms = all_terms_in_productions(&prods);
     ParserData {
         productions: prods,
         nonterms,
         terms,
+        terms_to_store,
     }
 }
 
-fn all_productions_in_ast(ast: &Start) -> Vec<Production> {
+fn all_productions_in_ast(rules: &Rules) -> Vec<Production> {
     todo!()
+}
+fn all_stored_terms_in_ast(store: &Store) -> HashSet<String> {
+    // should have caught if they weren't terminal identifers in internal_parser
+    // therefore, panics are a bug in my programming, not seen by users
+    let mut set = HashSet::new();
+    for (tok, attrstring, _) in store.0.iter() {
+        let Token::Term = tok else { panic!() };
+        let Attr::AttrString(term_to_store) = attrstring else {
+            panic!()
+        };
+        set.insert(term_to_store.clone());
+    }
+    set
 }
 fn all_nonterms_in_productions(prods: &Vec<Production>) -> Vec<String> {
     todo!()
@@ -30,4 +51,5 @@ pub struct ParserData {
     pub productions: Vec<Production>,
     pub nonterms: Vec<String>,
     pub terms: Vec<String>,
+    pub terms_to_store: HashSet<String>,
 }
