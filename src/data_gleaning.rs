84286d50334e5bde9cea6c441a428706f779b4f5
William Campbell <numbersdev@protonmail.com>
2026-08-02 20:25:03 -0500

    made data_gleaning.rs cleaner for actual work

--- src/data_gleaning.rs
+++ src/data_gleaning.rs
@@ -1,10 +1,33 @@
 use crate::internal_parser::Start;
 
-pub fn data_from_parse(ast: Start) -> (Vec<Production>, Vec<String>, Vec<String>) {
+pub fn data_from_parse(ast: &Start) -> ParserData {
+    let prods = all_productions_in_ast(ast);
+    let nonterms = all_nonterms_in_productions(&prods);
+    let terms = all_terms_in_productions(&prods);
+    ParserData {
+        productions: prods,
+        nonterms,
+        terms,
+    }
+}
+
+fn all_productions_in_ast(ast: &Start) -> Vec<Production> {
+    todo!()
+}
+fn all_nonterms_in_productions(prods: &Vec<Production>) -> Vec<String> {
+    todo!()
+}
+fn all_terms_in_productions(prods: &Vec<Production>) -> Vec<String> {
     todo!()
 }
 
 pub struct Production {
-    nonterm: String,
-    symbols: Vec<String>,
+    pub nonterm: String,
+    pub symbols: Vec<String>,
+}
+
+pub struct ParserData {
+    pub productions: Vec<Production>,
+    pub nonterms: Vec<String>,
+    pub terms: Vec<String>,
 }
