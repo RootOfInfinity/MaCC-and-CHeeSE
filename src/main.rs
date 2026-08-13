1e75f99c496806f76e8370b72e934708be7f3488
William Campbell <numbersdev@protonmail.com>
2026-08-13 11:17:02 -0500

    created the table creation and predictive parser runner files for LL(1) grammars

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod data_gleaning;
+// Will be replaced with generated Lexers and Parsers
 mod internal_lexer;
 mod internal_parser;
+// LL(1)
+mod ll1_parser;
+mod ll1_table_create;
 
 fn main() {
     println!("Hello, world!");
