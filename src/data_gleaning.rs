ecca5ade49b14b13536c01e0199605aacc9601b4
William Campbell <numbersdev@protonmail.com>
2026-08-13 21:52:45 -0500

    changed the data_gleaning file to turn both nonterms and terms into numbers for easier computation later

--- src/data_gleaning.rs
+++ src/data_gleaning.rs
@@ -1,16 +1,27 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::{
     internal_lexer::{Attr, Token},
-    internal_parser::{Rule, Rules, Start, Store, Symbol},
+    internal_parser::{Rule, Rules, Start, Store},
 };
 
 pub fn data_from_parse(ast: &Start) -> ParserData {
     let Start(store, rules) = ast;
-    let prods = all_productions_in_ast(rules);
-    let terms_to_store = all_stored_terms_in_ast(store);
-    let nonterms = all_nonterms_in_productions(&prods);
-    let terms = all_terms_in_productions(&prods);
+    let mut term_map = HashMap::new();
+    let mut nonterm_map = HashMap::new();
+
+    let prods = all_productions_in_ast(rules, &mut term_map, &mut nonterm_map);
+    let terms_to_store = all_stored_terms_in_ast(store, &term_map);
+
+    let mut terms = HashSet::new();
+    for term_num in term_map.values() {
+        terms.insert(Term(*term_num));
+    }
+    let mut nonterms = HashSet::new();
+    for nonterm_num in nonterm_map.values() {
+        nonterms.insert(Nonterm(*nonterm_num));
+    }
+
     ParserData {
         productions: prods,
         nonterms,
@@ -19,45 +30,87 @@ pub fn data_from_parse(ast: &Start) -> ParserData {
     }
 }
 
-fn all_productions_in_ast(rules: &Rules) -> Vec<Production> {
+fn all_productions_in_ast(
+    rules: &Rules,
+    term_map: &mut HashMap<String, i32>,
+    nonterm_map: &mut HashMap<String, i32>,
+) -> Vec<Production> {
     let mut prodvec = Vec::new();
+    let mut cur_term_id = 0;
+    let mut cur_nonterm_id = 0;
     for Rule(term, symlist, extrarule) in rules.0.iter() {
         let (Token::Nonterm, Attr::AttrString(nonterm), _) = term else {
             // can only get here with programmer error once error
             // handling is fixed in internal_parser and internal_lexer
             panic!();
         };
+        if !nonterm_map.contains_key(nonterm) {
+            nonterm_map.insert(nonterm.clone(), cur_nonterm_id);
+            cur_nonterm_id += 1;
+        }
         let mut symvec = Vec::new();
-        for Symbol(term) in symlist.0.iter() {
+        for crate::internal_parser::Symbol(term) in symlist.0.iter() {
             let (token, attr, _) = term;
             let Attr::AttrString(string) = attr else {
                 panic!();
             };
-            symvec.push((token.clone(), string.clone()));
+            match token {
+                Token::Nonterm => {
+                    if !nonterm_map.contains_key(string) {
+                        nonterm_map.insert(string.clone(), cur_nonterm_id);
+                        cur_nonterm_id += 1;
+                    }
+                    symvec.push(Symbol::N(Nonterm(*nonterm_map.get(string).unwrap())));
+                }
+                Token::Term => {
+                    if !term_map.contains_key(string) {
+                        term_map.insert(string.clone(), cur_term_id);
+                        cur_term_id += 1;
+                    }
+                    symvec.push(Symbol::T(Term(*term_map.get(string).unwrap())));
+                }
+                _ => (),
+            }
         }
         prodvec.push(Production {
-            nonterm: nonterm.clone(),
+            nonterm: Nonterm(*nonterm_map.get(nonterm).unwrap()),
             symbols: symvec,
         });
         // extra rules time
         for extra_symbols in extrarule.0.iter() {
             let mut symvec = Vec::new();
-            for Symbol(term) in extra_symbols.0.iter() {
+            for crate::internal_parser::Symbol(term) in extra_symbols.0.iter() {
                 let (token, attr, _) = term;
                 let Attr::AttrString(string) = attr else {
                     panic!();
                 };
-                symvec.push((token.clone(), string.clone()));
+                match token {
+                    Token::Nonterm => {
+                        if !nonterm_map.contains_key(string) {
+                            nonterm_map.insert(string.clone(), cur_nonterm_id);
+                            cur_nonterm_id += 1;
+                        }
+                        symvec.push(Symbol::N(Nonterm(*nonterm_map.get(string).unwrap())));
+                    }
+                    Token::Term => {
+                        if !term_map.contains_key(string) {
+                            term_map.insert(string.clone(), cur_term_id);
+                            cur_term_id += 1;
+                        }
+                        symvec.push(Symbol::T(Term(*term_map.get(string).unwrap())));
+                    }
+                    _ => (),
+                }
             }
             prodvec.push(Production {
-                nonterm: nonterm.clone(),
+                nonterm: Nonterm(*nonterm_map.get(nonterm).unwrap()),
                 symbols: symvec,
             });
         }
     }
     prodvec
 }
-fn all_stored_terms_in_ast(store: &Store) -> HashSet<String> {
+fn all_stored_terms_in_ast(store: &Store, term_map: &HashMap<String, i32>) -> HashSet<Term> {
     // should have caught if they weren't terminal identifers in internal_parser
     // therefore, panics are a bug in my programming, not seen by users
     let mut set = HashSet::new();
@@ -66,51 +119,61 @@ fn all_stored_terms_in_ast(store: &Store) -> HashSet<String> {
         let Attr::AttrString(term_to_store) = attrstring else {
             panic!()
         };
-        set.insert(term_to_store.clone());
+        set.insert(Term(*term_map.get(term_to_store).unwrap()));
     }
     set
 }
-fn all_nonterms_in_productions(prods: &Vec<Production>) -> HashSet<String> {
-    // get all the nonterms on the left side, but make sure there isn't
-    // any wild nonterms on the right side of productions by throwing
-    // error with panic and crashing
-    let mut nonterm_set = HashSet::new();
-    for prod in prods.iter() {
-        nonterm_set.insert(prod.nonterm.clone());
-    }
-    // check lap
-    for prod in prods.iter() {
-        for symbol in prod.symbols.iter() {
-            if let Token::Nonterm = symbol.0 {
-                if nonterm_set.contains(&symbol.1) {
-                    // does need to panic
-                    panic!("There is a nonterm without a definition");
-                }
-            }
-        }
-    }
-    nonterm_set
-}
-fn all_terms_in_productions(prods: &Vec<Production>) -> HashSet<String> {
-    let mut term_set = HashSet::new();
-    for prod in prods.iter() {
-        for symbol in prod.symbols.iter() {
-            if let Token::Term = symbol.0 {
-                term_set.insert(symbol.1.clone());
-            }
-        }
-    }
-    term_set
-}
+// Previous implementation inserted right into all_productions_in_ast
+// fn all_nonterms_in_productions(prods: &Vec<Production>) -> HashSet<String> {
+//     // get all the nonterms on the left side, but make sure there isn't
+//     // any wild nonterms on the right side of productions by throwing
+//     // error with panic and crashing
+//     let mut nonterm_set = HashSet::new();
+//     for prod in prods.iter() {
+//         nonterm_set.insert(prod.nonterm.clone());
+//     }
+//     // check lap
+//     for prod in prods.iter() {
+//         for symbol in prod.symbols.iter() {
+//             if let Token::Nonterm = symbol.0 {
+//                 if !nonterm_set.contains(&symbol.1) {
+//                     // does need to panic
+//                     panic!("There is a nonterm without a definition");
+//                 }
+//             }
+//         }
+//     }
+//     nonterm_set
+// }
+// fn all_terms_in_productions(prods: &Vec<Production>) -> HashSet<String> {
+//     let mut term_set = HashSet::new();
+//     for prod in prods.iter() {
+//         for symbol in prod.symbols.iter() {
+//             if let Token::Term = symbol.0 {
+//                 term_set.insert(symbol.1.clone());
+//             }
+//         }
+//     }
+//     term_set
+// }
 
 pub struct Production {
-    pub nonterm: String,
-    pub symbols: Vec<(Token, String)>,
+    pub nonterm: Nonterm,
+    pub symbols: Vec<Symbol>,
 }
 
 pub struct ParserData {
     pub productions: Vec<Production>,
-    pub nonterms: HashSet<String>,
-    pub terms: HashSet<String>,
-    pub terms_to_store: HashSet<String>,
+    pub nonterms: HashSet<Nonterm>,
+    pub terms: HashSet<Term>,
+    pub terms_to_store: HashSet<Term>,
+}
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+pub struct Term(i32);
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+pub struct Nonterm(i32);
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+pub enum Symbol {
+    T(Term),
+    N(Nonterm),
 }
